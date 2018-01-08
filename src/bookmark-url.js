ad7c68ad53b9781513141822cd8e9b08f4221dc8
mcsrambles <johnmccampbell@gmail.com>
2018-01-08 17:03:45 -0500

    Updated for new repo name

--- src/bookmark-url.js
+++ src/bookmark-url.js
@@ -2,10 +2,10 @@ javascript:(function() {
   var link = document.createElement('link');
   link.rel='stylesheet';
   link.type='text/css';
-  link.href = 'https://forbes.github.io/gpt-bookmarklet/src/gpt-styles.css';
+  link.href = 'https://forbes.github.io/ad-inspector/src/styles.css';
   document.head.appendChild(link);
 	var script = document.createElement('script');
-	script.src = 'https://forbes.github.io/gpt-bookmarklet/src/gpt-loader.js';
-	script.id = 'gpt-bookmarklet';
+	script.src = 'https://forbes.github.io/ad-inspector/src/loader.js';
+	script.id = 'ad-inspector';
 	document.body.appendChild(script);
 })();
