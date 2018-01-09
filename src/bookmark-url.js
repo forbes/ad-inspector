7b53d4ffd52ba0c19d0c00a821d8dddfef2e24d8
mcsrambles <johnmccampbell@gmail.com>
2018-01-09 17:31:53 -0500

    Move style tag creation into module itself

--- src/bookmark-url.js
+++ src/bookmark-url.js
@@ -1,9 +1,4 @@
 javascript:(function() {
-  var link = document.createElement('link');
-  link.rel='stylesheet';
-  link.type='text/css';
-  link.href = 'https://forbes.github.io/ad-inspector/src/styles.css';
-  document.head.appendChild(link);
 	var script = document.createElement('script');
 	script.src = 'https://forbes.github.io/ad-inspector/src/loader.js';
 	script.id = 'ad-inspector';
