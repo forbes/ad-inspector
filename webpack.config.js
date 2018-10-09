a67d2e529218a87cb7894e4da618f967183cc2ec
mcscrambles <johnmccampbell@gmail.com>
2018-10-09 16:49:10 -0400

    Update dependencies, formatting, and manifest version revert

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'src'),
@@ -10,18 +11,26 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: [/(node_modules)/, path.resolve(__dirname, 'src/background.js'), path.resolve(__dirname, 'src/loader.js')],
+                exclude: [
+                    /(node_modules)/,
+                    path.resolve(__dirname, 'src/background.js'),
+                    path.resolve(__dirname, 'src/loader.js'),
+                ],
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['babel-preset-env']
-                    }
-                }
+                        presets: ['babel-preset-env'],
+                    },
+                },
             },
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader", "postcss-loader"]
-            }
-        ]
-    }
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                ],
+            },
+        ],
+    },
 };
